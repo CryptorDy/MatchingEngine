3b724b4f7ea78a560c069d5fc7e120230961c53d
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2019-07-04 16:57:29 +0600

    add more unit test checks

--- Stock.Trading.Tests/MatchingUnitTest.cs
+++ Stock.Trading.Tests/MatchingUnitTest.cs
@@ -12,27 +12,23 @@ namespace Stock.Trading.Tests
     {
         public const string _currencyPairCode = "ETH_BTC";
 
-        public Order cheapBid = new Order(true, _currencyPairCode, 3, 10) { Fulfilled = 2, Blocked = 1 };
-        public Order cheapAsk = new Order(false, _currencyPairCode, 2, 10) { Fulfilled = 3, Blocked = 1 };
-        public Order expensiveAsk = new Order(false, _currencyPairCode, 5, 10) { Fulfilled = 4, Blocked = 1 };
+        public Order _cheapBid = new Order(true, _currencyPairCode, 3, 10);
+        public Order _cheapBidWithFulfilled = new Order(true, _currencyPairCode, 3, 10) { Fulfilled = 2 };
+        public Order _cheapBidWithBlocked = new Order(true, _currencyPairCode, 3, 10) { Blocked = 1 };
+        public Order _cheapBidWithFulfilledBlocked = new Order(true, _currencyPairCode, 3, 10) { Fulfilled = 3, Blocked = 1 };
 
-        [Fact]
-        public void CorrentMatchForSimpleImputReturnOneDeal()
-        {
-            var pool = new List<Order> { cheapBid };
+        public Order _cheapAsk = new Order(false, _currencyPairCode, 2, 10) { Fulfilled = 3, Blocked = 1 };
+        public Order _cheapAskWithFulfilled = new Order(false, _currencyPairCode, 2, 10) { Fulfilled = 2 };
+        public Order _cheapAskWithBlocked = new Order(false, _currencyPairCode, 2, 10) { Blocked = 1 };
+        public Order _cheapAskWithFulfilledBlocked = new Order(false, _currencyPairCode, 2, 10) { Fulfilled = 3, Blocked = 1 };
 
-            var service = new OrdersMatcher(null);
-
-            var result = service.Match(pool, cheapAsk);
-            Assert.Single(result.Deals);
-            Assert.Equal<List<Order>>(new List<Order>() { cheapBid, cheapAsk }, result.ModifiedOrders);
-        }
+        public Order _expensiveAsk = new Order(false, _currencyPairCode, 5, 10) { Fulfilled = 4, Blocked = 1 };
 
         [Fact]
         public void EmptyDataReturnEmptyResult()
         {
             var service = new OrdersMatcher(null);
-            var result = service.Match(new List<Order>(), new Order());
+            var result = service.Match(new List<Order>(), _cheapBid);
             Assert.Empty(result.Deals);
             Assert.Empty(result.ModifiedOrders);
         }
@@ -40,26 +36,49 @@ namespace Stock.Trading.Tests
         [Fact]
         public void UnmatchableOrdersDontMatch()
         {
-            var pool = new List<Order> { cheapBid };
+            var pool = new List<Order> { _cheapBid };
 
             var service = new OrdersMatcher(null);
 
-            var result = service.Match(pool, expensiveAsk);
+            var result = service.Match(pool, _expensiveAsk);
             Assert.Empty(result.Deals);
             Assert.Empty(result.ModifiedOrders);
         }
 
         [Fact]
-        public void DealHasLowestPriceAndRightAmount()
+        public void CorrectMatchFor2OrdersReturnOneDeal()
         {
-            var pool = new List<Order> { cheapBid };
+            var ordersToMatch = new List<Order> {
+                _cheapBid, _cheapAsk,
+                _cheapBidWithFulfilled, _cheapAskWithFulfilled,
+                _cheapBidWithBlocked, _cheapAskWithBlocked,
+                _cheapBidWithFulfilledBlocked, _cheapAskWithFulfilledBlocked
+            };
+            for (int i = 0; i < ordersToMatch.Count; i += 2)
+            {
+                Console.WriteLine($"Test Match for orders pair {i / 2}");
+                Order bid = ordersToMatch[0], ask = ordersToMatch[1];
+                var pool = new List<Order> { bid };
 
-            var service = new OrdersMatcher(null);
+                var service = new OrdersMatcher(null);
 
-            var result = service.Match(pool, cheapAsk);
-            Assert.Single(result.Deals);
-            Assert.Equal(result.Deals.First().Price, Math.Min(cheapBid.Price, cheapAsk.Price));
-            Assert.Equal(result.Deals.First().Amount, Math.Min(cheapBid.AvailableAmount, cheapAsk.AvailableAmount));
+                var result = service.Match(pool, ask);
+
+                decimal expectedDealVolume = Math.Min(bid.AvailableAmount, ask.AvailableAmount);
+                decimal expectedDealPrice = Math.Min(bid.Price, ask.Price);
+                Assert.Single(result.Deals);
+                Assert.Equal(result.Deals.First().Volume, expectedDealVolume);
+                Assert.Equal(result.Deals.First().Price, expectedDealPrice);
+
+                Order resultBid = result.ModifiedOrders.Single(_ => _.IsBid),
+                    resultAsk = result.ModifiedOrders.Single(_ => !_.IsBid);
+                Assert.Equal(resultBid.AvailableAmount, bid.AvailableAmount - expectedDealVolume);
+                Assert.Equal(resultAsk.AvailableAmount, ask.AvailableAmount - expectedDealVolume);
+                Assert.True(resultBid.Fulfilled <= resultBid.Amount);
+                Assert.True(resultAsk.Fulfilled <= resultAsk.Amount);
+                Assert.True(resultBid.Status == (resultBid.Fulfilled == resultBid.Amount ? OrderStatus.Completed : OrderStatus.Active));
+                Assert.True(resultAsk.Status == (resultAsk.Fulfilled == resultAsk.Amount ? OrderStatus.Completed : OrderStatus.Active));
+            }
         }
     }
 }
