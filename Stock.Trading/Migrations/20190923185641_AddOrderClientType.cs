ff291a7255bbbe82ec6823d4f4f09ff9a676d209
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2019-09-26 13:44:11 +0600

    add migration

---
+++ Stock.Trading/Migrations/20190923185641_AddOrderClientType.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stock.Trading.Migrations
+{
+    public partial class AddOrderClientType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FromInnerTradingBot",
+                table: "Bids");
+
+            migrationBuilder.DropColumn(
+                name: "FromInnerTradingBot",
+                table: "Asks");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClientType",
+                table: "Bids",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClientType",
+                table: "Asks",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientType",
+                table: "Bids");
+
+            migrationBuilder.DropColumn(
+                name: "ClientType",
+                table: "Asks");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "FromInnerTradingBot",
+                table: "Bids",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "FromInnerTradingBot",
+                table: "Asks",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
