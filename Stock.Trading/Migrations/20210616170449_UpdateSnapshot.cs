4be8984cad51653cc7884b05e2962313b7b0a050
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2021-06-16 23:05:32 +0600

    update db snapshot after ef update

---
+++ Stock.Trading/Migrations/20210616170449_UpdateSnapshot.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stock.Trading.Migrations
+{
+    public partial class UpdateSnapshot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
